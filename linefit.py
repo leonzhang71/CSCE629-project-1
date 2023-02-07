51165e65dbd884abb786f359034cf8ba5c970c37
leonzhang71 <112522298+leonzhang71@users.noreply.github.com>
2023-02-07 00:41:26 -0600

    Add files via upload

---
+++ linefit.py
@@ -0,0 +1,9 @@
+#examples_of_instances
+#first solution for reading the file
+#filepath = '../data/test.txt'
+#can switch the file name into filepath
+
+import pickle as pkl
+with open("examples_of_instances", 'rb') as f:
+    obj = pkl.load(f)
+    print(obj)
