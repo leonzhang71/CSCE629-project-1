f9ef1bf312afdff93f6e143d0f17e77d0f2e15a0
Chonglin Zhang <mu948146407@hotmail.com>
2023-02-07 19:39:39 -0600

    able to pick the value

--- linefit.py
+++ linefit.py
@@ -3,8 +3,18 @@
 #filepath = '../data/test.txt'
 #can switch the file name into filepath
 #hello
+#UnicodeDecodeError: 'utf-8' codec can't decode byte 0x80 in position 0: invalid start byte
+
 
 import pickle as pkl
 with open("examples_of_instances", 'rb') as f:
     obj = pkl.load(f)
-    print(obj)
+    #this part is to use obj["x_list"][0][0] to pick up the value for data file
+    print(obj["x_list"][0][0])
+
+
+
+'''
+f = open("examples_of_instances", "rb")
+print(f.read())
+'''
